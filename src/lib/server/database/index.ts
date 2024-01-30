25c7bf510afc69214de5e0757c78d322086f3552
Jason Kao <jason.kao@columbia.edu>
2024-01-29 23:36:07 -0500

    populated liveblocks with a blocks array

--- src/lib/server/database/index.ts
+++ src/lib/server/database/index.ts
@@ -1,13 +1,12 @@
 import { Liveblocks } from '@liveblocks/node';
 import { LIVEBLOCKS_SECRET_KEY } from '$env/static/private';
-import exampleData from './example-data.json';
 
 const liveblocks = new Liveblocks({ secret: LIVEBLOCKS_SECRET_KEY });
 const yjsDoc = await liveblocks.getYjsDocument('my-room');
 
 export function getBlocks(): Block[] {
 	// Map through the example data. If the block is a graphic block, set the code property to the code from the Yjs document. Otherwise, return the block as is.
-	const blocks = (exampleData as Array<TextBlock | { type: 'graphic'; name: string }>).map(
+	const blocks = (yjsDoc['blocks'] as Array<TextBlock | { type: 'graphic'; name: string }>).map(
 		(block) => {
 			if (block.type === 'graphic') {
 				return {
