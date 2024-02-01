d356abce632e8679c749b9ca7d786e67b42ffc1f
Jason Kao <jason.kao@columbia.edu>
2024-02-01 12:11:41 -0500

    test bundle

--- code-editor/scripts/bundle-editor/index.js
+++ code-editor/scripts/bundle-editor/index.js
@@ -13,9 +13,8 @@ import { execSync } from 'child_process';
 const cwd = '../text-editor';
 
 console.time('install');
-if (!fs.existsSync(cwd + '/node_modules')) execSync('npm ci', { cwd });
-// kill symlink
-execSync('ls node_modules/shared', { cwd, stdio: 'inherit' });
+// if (!fs.existsSync(cwd + '/node_modules')) execSync('npm ci', { cwd });
+execSync('rm -rf node_modules package-lock.json && npm ci', { cwd });
 console.timeEnd('install');
 
 const zip = new AdmZip();
