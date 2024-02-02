7aab5e1b1fdc8986b20d9cf42d02146fcb1e9239
Jason Kao <jason.kao@columbia.edu>
2024-02-02 18:39:22 -0500

    fixed double mount on internal dev server

--- text-editor/vite.config.js
+++ text-editor/vite.config.js
@@ -4,7 +4,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		include: ['prosemirror-state', 'prosemirror-transform', 'prosemirror-model', 'prosemirror-view', 'shared']
+		include: [
+			'prosemirror-state',
+			'prosemirror-transform',
+			'prosemirror-model',
+			'prosemirror-view',
+			'shared' // prevents double-mounting from vite optimizing
+		]
 	},
 	// Normally this would be unnecessary, but we
 	// need it for learn.svelte.dev
