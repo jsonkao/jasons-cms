006ce4c341f019079d97e1503549cd50441478d2
Jason Kao <jason.kao@columbia.edu>
2024-02-09 15:25:20 -0500

    la 5th demo; use local file as seed

--- text-editor/src/lib/generated/graphic1.svelte
+++ text-editor/src/lib/generated/graphic1.svelte
@@ -1,28 +1,80 @@
 <script>
-	/** @type {import('yjs').Map<any>} */
-	export let prose;
+	// SEED REMOVE START
+	/** @type {import('yjs').Map<any>} */ export let prose;
+	import Editable from '$lib/components/Editable.svelte';
+	// SEED REMOVE END
 
-	import { onMount } from 'svelte';
+	const copy = [
+		'Years ago, the all-white judges of a Louisiana appellate court decided, in secret, to systematically ignore petitions filed by prisoners, most of them Black, who claimed they had been unjustly convicted.',
+		'This is the story of a horrendous injustice and the three people who tried to expose it. It begins with a suicide note.'
+	];
 
-	let count = 0;
+	const images = [
+		'PPUB-001-louisiana_fifth-papers-1',
+		'PPUB-001-louisiana_fifth-papers-2',
+		'PPUB-001-louisiana_fifth-papers-3'
+	];
 
-	onMount(() => {
-		const interval = setInterval(() => (count += 1), 1000);
-		return () => clearInterval(interval);
-	});
+	let scrollY = 0;
 </script>
 
-<div>
-	<p>Nice: {count}</p>
+<svelte:window bind:scrollY />
+
+<div class="paper-opener">
+	<div class="images">
+		<img
+			src="https://static.propublica.org/projects/graphics/2023-la-5th-circuit/backgrounds/1200.webp"
+		/>
+
+		{#each images as name, i}
+			<img
+				src="https://static.propublica.org/projects/graphics/2023-la-5th-circuit/layers/{name}.webp"
+				style="transform: translateY({(0.15 + i * 0.1) * scrollY}px);"
+			/>
+		{/each}
+	</div>
+
+	<div class="copy">
+		{#each [0, 1] as index}
+			<div style="top: {index === 0 ? 6 : 53}%">
+				<p>{copy[index]}</p>
+				<!-- <Editable {prose} key={index.toString()} /> -->
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-	div {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 300px;
-		background: rgba(255, 0, 0, 0.1);
-		border: 1px solid red;
+	.paper-opener,
+	.images {
+		display: grid;
+	}
+
+	.images > *,
+	.paper-opener > * {
+		grid-row: 1;
+		grid-column: 1;
+	}
+
+	img {
+		width: 100%;
+		aspect-ratio: 1 / 2.335;
+	}
+
+	.copy {
+		width: 100%;
+		position: relative;
+		text-align: center;
+	}
+
+	.copy div {
+		position: absolute;
+		max-width: 520px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.copy div:last-child {
+		color: white;
 	}
 </style>
