634c986aed733b984f00d106113717b71710bff4
Jason Kao <jason.kao@columbia.edu>
2024-02-11 15:22:41 -0500

    env test

--- text-editor/svelte.config.js
+++ text-editor/svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+console.log(process.env.VERCEL_URL)
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
