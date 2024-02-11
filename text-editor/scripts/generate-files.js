854ff5a53f65dec0d4a747c46a8db6efa2e3a3ef
Jason Kao <jason.kao@columbia.edu>
2024-02-11 15:03:29 -0500

    test script

---
+++ text-editor/scripts/generate-files.js
@@ -0,0 +1,45 @@
+import { Liveblocks as LiveblocksNode } from '@liveblocks/node';
+import * as Y from 'yjs';
+import fs from 'fs/promises';
+import { existsSync } from 'fs';
+import { resolve, join } from 'path';
+
+main().catch(console.error);
+
+async function main() {
+	const { SLUG: slug, LIVEBLOCKS_SECRET_KEY } = process.env;
+
+	if (slug === undefined) throw new Error('SLUG environment variable is not set');
+	if (LIVEBLOCKS_SECRET_KEY === undefined)
+		throw new Error('LIVEBLOCKS_SECRET_KEY environment variable is not set');
+
+	const liveblocks = new LiveblocksNode({
+		secret: LIVEBLOCKS_SECRET_KEY
+	});
+
+	const ydoc = new Y.Doc();
+	const update = await liveblocks.getYjsDocumentAsBinaryUpdate(slug);
+	Y.applyUpdate(ydoc, new Uint8Array(update));
+
+	const blocks = ydoc.getArray('blocks').toJSON();
+	const graphicBlocks = blocks.filter(({ type }) => type === 'graphic');
+	const path = resolve(__dirname, '../src/routes/render/[slug]/generated');
+	if (!existsSync(path)) await fs.mkdir(path);
+
+	console.log(path);
+	console.log(graphicBlocks.map((block) => join(path, `${block.name}.svelte`)));
+
+	// Save all Svelte components to disk
+	await Promise.all([
+		...graphicBlocks.map((block) => fs.writeFile(join(path, `${block.name}.svelte`), block.code)),
+		fs.writeFile(
+			join(path, 'index.js'),
+			graphicBlocks.map(({ name }) => `import ${name} from './${name}.svelte';`).join('\n') +
+				`\nexport default { ${graphicBlocks.map(({ name }) => name).join(', ')} };`
+		)
+	]);
+
+	return {
+		blocks
+	};
+}
--- text-editor/src/routes/render/[slug]/+page.server.js
+++ text-editor/src/routes/render/[slug]/+page.server.js
@@ -1,9 +1,6 @@
 import { Liveblocks as LiveblocksNode } from '@liveblocks/node';
 import { LIVEBLOCKS_SECRET_KEY } from '$env/static/private';
 import * as Y from 'yjs';
-import fs from 'fs/promises';
-import { existsSync } from 'fs';
-import { resolve, join } from 'path';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
@@ -16,23 +13,6 @@ export async function load({ params }) {
 	Y.applyUpdate(ydoc, new Uint8Array(update));
 
 	const blocks = ydoc.getArray('blocks').toJSON();
-	const graphicBlocks = blocks.filter(({ type }) => type === 'graphic');
-	const path = resolve(__dirname, 'generated');
-	if (!existsSync(path)) await fs.mkdir(path);
-
-	console.log(path);
-	console.log(graphicBlocks.map((block) => join(path, `${block.name}.svelte`)));
-
-	// Save all Svelte components to disk
-	await Promise.all([
-		...graphicBlocks.map((block) => fs.writeFile(join(path, `${block.name}.svelte`), block.code)),
-		fs.writeFile(
-			join(path, 'index.js'),
-			graphicBlocks.map(({ name }) => `import ${name} from './${name}.svelte';`).join('\n') +
-				`\nexport default { ${graphicBlocks.map(({ name }) => name).join(', ')} };`
-		)
-	]);
-
 	return {
 		blocks
 	};
