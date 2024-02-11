651a092f3a77032f09f4b712aa46501f1b095efb
Jason Kao <jason.kao@columbia.edu>
2024-02-11 15:55:40 -0500

    testing vercel

--- code-editor/src/routes/preview/[slug]/+page.server.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
-  return {
-    renderPath: `/render/${params.slug}`
-  }
-}
--- code-editor/src/routes/preview/[slug]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	export let data;
-</script>
-
-<iframe src="{data.renderPath}" frameborder="0" style="width: 100%; height: 100%;"></iframe>
