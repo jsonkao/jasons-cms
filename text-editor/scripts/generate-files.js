308da6d9718db77e96cd026d0963aae00485b80d
Jason Kao <jason.kao@columbia.edu>
2024-02-11 23:33:29 -0500

    try again

--- text-editor/scripts/generate-files.js
+++ text-editor/scripts/generate-files.js
@@ -74,7 +74,7 @@ async function generateFiles(liveblocks, generatedPath, slug) {
  */
 function hackyModifications(code) {
 	// Add crossorigin="anonymous" to all img tags
-	code = code.replace(/<img\W/g, '<img\Wcrossorigin="anonymous" ');
+	code = code.replace(/<img\W/g, '<img crossorigin="anonymous" ');
 	return code;
 }
 
