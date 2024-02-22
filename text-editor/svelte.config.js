26fea042d55a396f5a0bb7e8a285807f4277ef12
Jason Kao <jason.kao@columbia.edu>
2024-02-22 16:36:53 -0500

    verfcelii

--- text-editor/svelte.config.js
+++ text-editor/svelte.config.js
@@ -8,6 +8,7 @@ const config = {
 		alias: {
 			$shared: !!process.env.VERCEL ? './shared' : '../shared'
 		},
+
 		// For the tutorial, we need to disable CSRF protection.
 		// Don't do this in your own apps unless you know what you're doing!
 		// See https://kit.svelte.dev/docs/configuration#csrf for more info.
